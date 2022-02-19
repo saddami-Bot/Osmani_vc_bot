9e9bb47b337bf40f2d7e0541afebce35792d841b
Searosan <100029620+Searosan@users.noreply.github.com>
2022-02-19 20:21:54 +0300

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -29,7 +29,7 @@ print(f"[INFO]: ASSISTANT STARTED AS {ASSNAME}!")
 async def load_start():
     restart_data = await clean_restart_stage()
     if restart_data:
-        print("[INFO]: SENDING RESTART STATUS TO ZAID SERVER")
+        print("[INFO]: SENDING RESTART STATUS TO OSMANI BOT")
         try:
             await app.edit_message_text(
                 restart_data["chat_id"],
@@ -52,8 +52,8 @@ async def load_start():
             print("Error came while clearing db")
             pass     
     await app.send_message(LOG_GROUP_ID, "Music Bot Started")
-    await client.send_message(LOG_GROUP_ID, "Assistant Of Zaid Music Started")
-    print("[INFO]: STARTED THE ZAID BOT AND SENDING THE INFO TO ZAID SERVER")
+    await client.send_message(LOG_GROUP_ID, "Assistant Of Osmani Music Started")
+    print("[INFO]: STARTED THE OSMANI BOT AND SENDING THE INFO TO OSMANI BOT")
     
    
 loop = asyncio.get_event_loop()
